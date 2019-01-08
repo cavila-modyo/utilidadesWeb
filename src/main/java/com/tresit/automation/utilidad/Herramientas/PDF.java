4f0f0fbff05e11a0e2db2f67d588a622a111f265
jonathan.valdes <50*2=cien>
2019-01-08 16:33:14 -0300

    Se agrega varsion de compiler en pom Se Modifica Clase PDF. Para Generar archivos PDF con imagenes contenidas en una carpeta indicada por parametro.

--- src/main/java/com/tresit/automation/utilidad/Herramientas/PDF.java
+++ src/main/java/com/tresit/automation/utilidad/Herramientas/PDF.java
@@ -3,28 +3,24 @@ package com.tresit.automation.utilidad.Herramientas;
 import org.apache.commons.io.FileUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
-import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.graphics.xobject.PDJpeg;
-import org.apache.pdfbox.pdmodel.graphics.xobject.PDXObjectImage;
-import org.apache.pdfbox.util.PDFImageWriter;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
 import javax.imageio.ImageIO;
-import javax.swing.text.Document;
-import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.*;
-import java.nio.file.Files;
+import java.io.File;
+import java.io.FileInputStream;
 
 public class PDF {
 
-    /*
-    public static void main(String [] Args){
+
+    /*public static void main(String [] Args){
         String carpeta = "C:\\Automatizacion\\ATC\\Imagenes";
         String nombrePDF = "ArchivoPrueba";
         GeneratePDF_Images(carpeta, nombrePDF);
-    }
-    */
+    }*/
+
 
     public static void GeneratePDF_Images(String carpeta, String NombreArchivo) {
         try {
@@ -44,7 +40,9 @@ public class PDF {
                 float height = bimg.getHeight();
                 PDPage page = new PDPage(new PDRectangle(width, height));
                 document.addPage(page);
-                PDXObjectImage img = new PDJpeg(document, bimg);
+
+                PDImageXObject img = PDImageXObject.createFromFileByContent(imagen, document);
+
                 PDPageContentStream contentStream = new PDPageContentStream(document, page);
                 contentStream.drawImage(img, 0, 0);
                 contentStream.close();
