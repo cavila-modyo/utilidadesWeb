140ca6de133e60ee14f335ace124a022ddfb05b1
jonathan.valdes <50*2=cien>
2019-01-09 12:39:02 -0300

    Se agrega fecha y hora por cada linea que se agrega al log de ejecución.

--- src/main/java/com/tresit/automation/utilidad/Herramientas/Log.java
+++ src/main/java/com/tresit/automation/utilidad/Herramientas/Log.java
@@ -12,7 +12,7 @@ public class Log {
 
     // Como utilizar el loguer. Crea Archivo si no existe para el dia de la ejecucion.
     // Inserta fila con la data que se pasa por parametro.
-    // Dentro de la data que se debe envia, debemos informar el usuario, la fehca, el caso de prueba ejecutado
+    // Dentro de la data que se debe envia, debemos informar el usuario, la fecha, el caso de prueba ejecutado
     // Los endpoint consumidos y las respuestas.
     public void AgregarLoguerDia(String data, String carpeta){
         try {
@@ -24,7 +24,7 @@ public class Log {
             if (!folder.isDirectory()) {
                 folder.mkdirs();
             }
-
+            FechaHoraActual fech = new FechaHoraActual();
             File file = new File(carpeta + "\\app_log.log");
             // Si el archivo no existe, se crea!
             if (!file.exists()) {
@@ -33,7 +33,7 @@ public class Log {
             // flag true, indica adjuntar información al archivo.
             fw = new FileWriter(file.getAbsoluteFile(), true);
             bw = new BufferedWriter(fw);
-            bw.write("" + "\n" + data);
+            bw.write("" + "\n" + fech.getFecha() + " " + fech.getHora() + " : " + data);
             //System.out.println("Se agrega fila en archivo: " + ruta);
         } catch (IOException e) {
             e.printStackTrace();
